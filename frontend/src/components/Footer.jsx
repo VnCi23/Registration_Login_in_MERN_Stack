82203f0978b7db17034d47299cc5965eac74cb5f
Your Name <vincegaurino@gmail.com>
2024-02-24 17:28:15 +0800

    gg

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             
             <div className="text-center">
                     <h6>
-                        Made with by: <a href="#" style={{textDecoration : "none", color: "blue"}}>VnCi</a>
+                        Made with by: <a href="https://github.com/VnCi23" style={{textDecoration : "none", color: "blue"}}>VnCi</a>
                     </h6>
             </div> 
 
@@ -15,13 +15,14 @@ export default function Footer() {
                     <i className="bi bi-linkedin mx-2" style={{fontSize : "20px"}}></i>
                 </a>
 
+                <a href="https://vnci23.github.io/link/" target="_blank" rel="noreferrer">
+                    <i className="bi bi-globe mx-2" style={{fontSize : "21px"}}></i>
+                </a>
+
                 <a href="https://github.com/VnCi23" target="_blank" rel="noreferrer">
                     <i className="bi bi-github mx-2" style={{fontSize : "21px"}}></i>
                 </a>
 
-                <a href="vincechristiangaurino@outlook.ph" target="_blank" rel="noreferrer">
-                    <i className="bi bi-envelope-fill mx-2" style={{fontSize : "21px"}}></i>
-                </a>
             </div>
             {/* <footer style={{backgroundColor: 'crimson', color:'springgreen', textAlign: 'center', position:'fixed', width: '100%',height : '50px', bottom : '0', left: '0', right: '0', zIndex : '999'}}><h1>THIS IS MY FOOTER</h1></footer> */}
         </div>
